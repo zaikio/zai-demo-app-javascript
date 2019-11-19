53252760e18f000818a3dcf0b37e00855adfe326
Jalyna <jalyna.schroeder@gmail.com>
2019-11-19 10:14:10 +0100

    Use axios for demo

--- src/index.js
+++ src/index.js
@@ -1,39 +1,55 @@
-import { JSO } from "jso";
 import pkceChallenge from "pkce-challenge";
+import axios from "axios";
 
-const { code_verifier, code_challenge } = pkceChallenge();
+async function oauth2Callback(code) {
+  const code_verifier = window.sessionStorage.getItem("code_verifier");
+  window.sessionStorage.removeItem("code_verifier");
+  const response = await axios.post(
+    process.env.DIRECTORY_HOST + "/oauth/access_token.json",
+    {
+      client_id: process.env.DIRECTORY_OAUTH_CLIENT_ID,
+      client_secret: process.env.DIRECTORY_OAUTH_CLIENT_SECRET,
+      code_verifier,
+      code
+    }
+  );
 
-// Configure client
-const client = new JSO({
-  providerID: "hc-directory",
-  client_id: process.env.DIRECTORY_OAUTH_CLIENT_ID,
-  client_secret: process.env.DIRECTORY_OAUTH_CLIENT_SECRET,
-  redirect_uri: process.env.DIRECTORY_REDIRECT_URI,
-  authorization: "https://directory.heidelberg.cloud/oauth/authorize",
-  token: "https://directory.heidelberg.cloud/oauth/access_token",
-  debug: true,
-  request: {
-    code_challenge_method: "S256",
-    code_challenge
-  }
-  // TODO: scopes: { request: [] }
-});
-
-// On the page that the user is sent back to after authorization
-client.callback();
+  window.sessionStorage.setItem(
+    "directory_authentication",
+    JSON.stringify(response.data)
+  );
+  window.location.href = "/";
+}
 
 const login = document.getElementById("login");
 
-login.addEventListener("click", () => {
-  client
-    .getToken({
-      response_type: "application/json",
-      request: {
-        code_verifier
-      }
-    })
-    .then(token => {
-      console.log("Bearer token: ", token);
-      // TODO fetch https://directory.heidelberg.cloud/api/v1/person
+if (window.location.search.includes("code=")) {
+  oauth2Callback(window.location.search.replace("?code=", "").split("&")[0]);
+} else if (window.sessionStorage.getItem("directory_authentication")) {
+  const data = JSON.parse(
+    window.sessionStorage.getItem("directory_authentication")
+  );
+  console.log("LOGGED IN", data);
+  axios.defaults.headers.common["Authorization"] =
+    "Bearer " + data.access_token;
+  axios
+    .get(process.env.DIRECTORY_HOST + "/api/v1/person.json")
+    .then(response => {
+      console.log("AS", response.data);
+      document.body.innerHTML = `<div>Hello, ${response.data.full_name}</div>`;
     });
+}
+
+login.addEventListener("click", () => {
+  const { code_verifier, code_challenge } = pkceChallenge();
+  // Store code_verifier
+  window.sessionStorage.setItem("code_verifier", code_verifier);
+  // Open the page in a new window, then redirect back to a page that calls `oauth2Callback`.
+  window.open(
+    process.env.DIRECTORY_HOST +
+      "/oauth/authorize?client_id=" +
+      process.env.DIRECTORY_OAUTH_CLIENT_ID +
+      "&code_challenge_method=S256&code_challenge=" +
+      code_challenge
+  );
 });
