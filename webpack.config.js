68bdf007f1812728ef5e958e98b98ad8840dce31
Jalyna <jalyna.schroeder@gmail.com>
2019-11-27 11:55:14 +0100

    Update config for heroku deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,39 @@
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const webpack = require("webpack");
 
-module.exports = {
-  entry: './src/index.js',
+let config = {
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist/assets'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist/assets")
   },
 
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    publicPath: '/assets/',
+    contentBase: path.join(__dirname, "dist"),
+    publicPath: "/assets/",
     open: true,
     historyApiFallback: true
-  },
-
-  plugins: [
-    new Dotenv()
-  ]
+  }
 };
+
+if (process.env.NODE_ENV === "development") {
+  config.plugins = [new Dotenv()];
+} else {
+  config.plugins = [
+    new webpack.DefinePlugin({
+      "process.env.DIRECTORY_OAUTH_CLIENT_ID": JSON.stringify(
+        process.env.DIRECTORY_OAUTH_CLIENT_ID
+      ),
+      "process.env.DIRECTORY_OAUTH_CLIENT_SECRET": JSON.stringify(
+        process.env.DIRECTORY_OAUTH_CLIENT_SECRET
+      ),
+      "process.env.DIRECTORY_REDIRECT_URI": JSON.stringify(
+        process.env.DIRECTORY_REDIRECT_URI
+      ),
+      "process.env.DIRECTORY_HOST": JSON.stringify(process.env.DIRECTORY_HOST)
+    })
+  ];
+}
+
+module.exports = config;
