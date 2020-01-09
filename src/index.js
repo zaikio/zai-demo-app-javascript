ddc3aaa7817b7a6cfd1e8e0a108572c2a25352a1
Jalyna <jalyna.schroeder@gmail.com>
2020-01-09 18:37:23 +0100

    Adjust launchpad config

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ if (window.location.search.includes("code=")) {
         "authenticated_area_header"
       ).innerHTML = `<div>Welcome, ${response.data.full_name}</div>`;
       if (window.zaiLaunchpad) {
-        window.zaiLaunchpad.setup({ personData: response.data, directoryHost: process.env.DIRECTORY_HOST });
+        window.zaiLaunchpad.setup({ loadPersonData: () => response.data, directoryHost: process.env.DIRECTORY_HOST });
       }
     });
 
