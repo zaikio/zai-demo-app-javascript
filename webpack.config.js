b1ffeb1a142d7b45a8dd14b69eab576eddbb4f85
Jalyna <jalyna.schroeder@gmail.com>
2021-01-04 11:40:37 +0100

    Upgrade to latest webpack version

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 const path = require("path");
 const webpack = require("webpack");
-const Dotenv = require("dotenv-webpack");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const dotenv = require("dotenv");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const LAUNCHPAD_URL = process.env.LAUNCHPAD_URL || 'https://launchpad.zaikio.com/launchpad.js';
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+const LAUNCHPAD_URL =
+  process.env.LAUNCHPAD_URL || "https://launchpad.zaikio.com/launchpad.js";
 
 let config = {
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
 
   plugins: [
@@ -16,19 +19,32 @@ let config = {
       hash: true,
       template: "src/index.html",
       filename: "index.html",
-      launchpadUrl: LAUNCHPAD_URL
-    })
+      launchpadUrl: LAUNCHPAD_URL,
+    }),
   ],
 
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     open: true,
-    historyApiFallback: true
-  }
+    historyApiFallback: true,
+  },
+
+  resolve: {
+    fallback: {
+      crypto: require.resolve("crypto-browserify"),
+      buffer: require.resolve("buffer/"),
+      stream: require.resolve("stream-browserify"),
+      process: require.resolve("process"),
+    },
+  },
 };
 
 if (process.env.NODE_ENV === "development") {
-  config.plugins.push(new Dotenv());
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      process: { env: JSON.stringify(dotenv.config().parsed) },
+    })
+  );
 } else {
   config.plugins.push(
     new webpack.DefinePlugin({
@@ -41,7 +57,7 @@ if (process.env.NODE_ENV === "development") {
       "process.env.DIRECTORY_REDIRECT_URI": JSON.stringify(
         process.env.DIRECTORY_REDIRECT_URI
       ),
-      "process.env.DIRECTORY_HOST": JSON.stringify(process.env.DIRECTORY_HOST)
+      "process.env.DIRECTORY_HOST": JSON.stringify(process.env.DIRECTORY_HOST),
     })
   );
 }
